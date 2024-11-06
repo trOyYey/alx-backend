c58123734cea6bd75d72f046eb7c4ba29794a6b4
trOyYey <troyzuwara@gmail.com>
2024-11-06 00:47:43 +0000

    task3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" 3-app task """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ class that contains babel config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ 3-app
+    """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get_local function to determine
+    the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
